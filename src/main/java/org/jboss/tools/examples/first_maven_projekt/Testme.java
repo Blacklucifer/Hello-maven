5c29f2792333317374e5c7bad57094a14f608e8c
Christoph Marketsmüller <c.marketsmueller@gmail.com>
2017-01-19 21:40:20 +0100

    maven test

---
+++ src/main/java/org/jboss/tools/examples/first_maven_projekt/Testme.java
@@ -0,0 +1,10 @@
+package org.jboss.tools.examples.first_maven_projekt;
+
+public class Testme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("are you realy testing me?");
+	}
+
+}
